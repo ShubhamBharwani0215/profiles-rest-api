84b9a5b3440ef3f3b030b6fdcd7a7e6d127d2181
Shubham <jbsbrocks@gmail.com>
2024-10-02 17:07:22 +0530

    Added viewsets to profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 from profiles_api import serializers
+
 # Create your views here.
 
 class HelloApiView(APIView):
@@ -30,3 +32,36 @@ class HelloApiView(APIView):
         
     def delete(self, request, pk=None):
         return Response({"method": 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    '''Test API Viewset'''
+    serializer_class = serializers.HelloSerializer
+    def list(self, request):
+        a_viewset = [
+            'Uses actions like list, create, retrieve, update, partial_update',
+            'Automatically maps URLs using Routers'
+        ]
+        return Response({"mesage":'Hello',"a_viewset":a_viewset})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+    def retrieve(self,request,pk=None):
+        return Response({"http_method":'GET'})
+    
+    def update(self, request, pk=None):
+        return Response({"http_method":'PUT'})
+    
+    def partial_update(self, request, pk=None):
+        return Response({"http_method":'PATCH'})
+    
+    def destroy(self,request,pk=None):
+        return Response({'http_method':'DELETE'})
+            
